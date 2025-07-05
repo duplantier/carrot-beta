8c2c0aecc9b7753b7cc2135ad877206b196899ec
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2025-07-05 23:40:05 +0200

    [Refactor]: Homepage view

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -5,22 +5,20 @@ import { Transaction } from "@/components/Transaction";
 import { UserInfo } from "@/components/UserInfo";
 import { Verify } from "@/components/Verify";
 import { ViewPermissions } from "@/components/ViewPermissions";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Marble, TopBar } from "@worldcoin/mini-apps-ui-kit-react";
 import { MiniKit } from "@worldcoin/minikit-js";
 import { ChevronRight, Hammer, Target, Trophy } from "lucide-react";
 import { GlassCard } from "@/components/glass-card";
 import Link from "next/link";
 import Image from "next/image";
+import ComingSoon from "@/components/coming-soon";
 
 const features = [
-  {
-    icon: <Trophy className="w-8 h-8" />,
-    title: "Live Quizzes",
-    description:
-      "Join real-time quiz games with live leaderboards. Compete with others and win WLD/USDC prizes!",
-    href: "/quizzes",
-    color: "from-yellow-500 to-orange-500",
-  },
   {
     icon: <Target className="w-8 h-8" />,
     title: "Quests",
@@ -31,10 +29,18 @@ const features = [
   },
   {
     icon: <Hammer className="w-8 h-8" />,
-    title: "Build Challenges",
+    title: "Stake & Build & Earn",
     description:
       "Participate in hackathon-style challenges, creating innovative projects and competing for prizes that showcase your skills.",
     href: "/challenges",
+    color: "from-yellow-500 to-orange-500",
+  },
+  {
+    icon: <Trophy className="w-8 h-8" />,
+    title: "Live Quizzes",
+    description:
+      "Join real-time quiz games with live leaderboards. Compete with others and win WLD/USDC prizes!",
+    href: "/quizzes",
     color: "from-yellow-500 to-orange-500",
   },
 ];
@@ -45,29 +51,25 @@ export default async function Home() {
 
   return (
     <>
-      <Page.Header className="p-0 bg-white">
-        <TopBar
-          logo={
-            <Image
-              src="/carrot-logo.svg"
-              alt="logo"
-              width={100}
-              height={100}
-              className="w-10 h-10 object-contain"
-            />
-          }
-          endAdornment={
-            <div className="flex items-center gap-2">
-              <p className="text-sm font-semibold capitalize text-gray-900">
-                {session?.user.username}
-              </p>
-              <Marble src={session?.user.profilePictureUrl} className="w-12" />
-            </div>
-          }
-        />
-      </Page.Header>
-      <Page.Main className="container mx-auto px-4 py-6 md:py-8 flex justify-center items-center flex-col">
-        <div className="text-center mb-12">
+      <header className="px-6 py-4  flex justify-between items-center">
+        <Link href="/home">
+          <Image
+            src="/carrot-logo.svg"
+            alt="logo"
+            width={120}
+            height={120}
+            className=" object-contain"
+          />
+        </Link>
+        <div className="flex items-center gap-2">
+          <p className="text-sm font-semibold capitalize text-gray-900">
+            {session?.user.username || "Username"}
+          </p>
+          <Marble src={session?.user.profilePictureUrl} className="w-12" />
+        </div>
+      </header>
+      <Page.Main className="container mx-auto px-4 py-12 flex justify-center items-center flex-col">
+        <div className="text-center mb-12 mt-16">
           <h1>test</h1>
           <p>Username from MiniKit: {user?.username}</p>
           <p>Wallet Address from MiniKit: {user?.walletAddress}</p>
@@ -85,7 +87,7 @@ export default async function Home() {
           {/* Features Grid */}
           <div className="grid gap-6 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto">
             {features.map((feature, index) => (
-              <div key={feature.title}>
+              <div key={index}>
                 <GlassCard className="p-6 h-full" gradient hover>
                   <div className="flex flex-col h-full ">
                     <div
@@ -96,24 +98,29 @@ export default async function Home() {
 
                     <div className="flex-1">
                       <div className="flex items-center justify-between mb-3">
-                        <h3 className="text-xl font-bold ">{feature.title}</h3>
+                        <h3 className="text-xl font-extrabold flex items-center">
+                          {feature.title}
+                          {feature.title === "Live Quizzes" && (
+                            <ComingSoon description="Live Quizzes are not available yet! When they are available, you will be able to join them and compete with others for prizes." />
+                          )}
+                        </h3>
                       </div>
 
-                      <p className=" text-sm leading-relaxed mb-6">
+                      <p className=" text-sm text-left mb-8 leading-relaxed ">
                         {feature.description}
                       </p>
                     </div>
 
                     <Link
-                      href={feature.href}
-                      className="mt-auto cursor-pointer"
+                      href={
+                        feature.title === "Live Quizzes" ? "#" : feature.href
+                      }
+                      className="w-full bg-gradient-to-r from-yellow-500 to-orange-500 border  backdrop-blur-sm py-2 text-white font-semibold rounded-xl flex justify-center items-center gap-2 cursor-pointer"
                     >
-                      <button className="w-full bg-gradient-to-r from-yellow-500 to-orange-500 border cursor-pointer  text-white backdrop-blur-sm py-3">
-                        <span>Get Started</span>
-                        <div className="ml-0">
-                          <ChevronRight className="w-4 h-4" />
-                        </div>
-                      </button>
+                      <span className="text-white">Get Started</span>
+                      <div className="ml-0">
+                        <ChevronRight className="w-4 h-4 text-white" />
+                      </div>
                     </Link>
                   </div>
                 </GlassCard>
